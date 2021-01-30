d38aa4f2483c4ea8cd3624e24e137e9346bb7003
holidayonmail@gmail.com <holidayonmail@gmail.com>
2021-01-30 22:26:22 +0300

    Created new project

---
+++ src/components/counters.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+
+class Counters extends Component {
+    state = {}
+    render() {
+        return(<div><Counter></Counter><Counter></Counter><Counter></Counter><Counter></Counter></div>)
+    }
+
+
+}
+
+export default Counters
