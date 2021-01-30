9053c50d48203d3a1e989d4f1d877cdbbc50b09e
holidayonmail@gmail.com <holidayonmail@gmail.com>
2021-01-30 23:24:18 +0300

    Created new project

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,68 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() {
-        return (<h1>Hello World</h1>)
-    }
+  state = {
+    value: this.props.value,
+  };
+  //   tags: ["tag1", "tag2", "tag3"],
+  // };
+
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  // style = {
+  //   fontSize: 10,
+  //   fontWeight: "bold",
+  // };
+  handleIncrement = () => {
+    // console.log("Increment Clicked", product);
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  // doHandleIncrement = () => {
+  //   this.handleIncrement({ id: 1 });
+  // };
+
+  render() {
+    // console.log("props", this.props);
+    return (
+      <div>
+        {/*{this.state.tags.length === 0 && "Please create a new tag"}*/}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {/*<ul>{this.renderTags()}</ul>*/}
+      </div>
+    );
+  }
+
+  // renderTags() {
+  //   if (this.state.tags.lengh === 0) return <p>There are no tags</p>;
+  //   return (
+  //     <ul>
+  //       {this.state.tags.map((tag) => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    // const { count } = this.state.count;
+    return this.state.value === 0 ? <h1>Zero</h1> : this.state.value;
+  }
 }
 
-export default Counter
+export default Counter;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,13 +1,29 @@
-import React, {Component} from 'react'
-
+import React, { Component } from "react";
+import Counter from "./counter";
 
 class Counters extends Component {
-    state = {}
-    render() {
-        return(<div><Counter></Counter><Counter></Counter><Counter></Counter><Counter></Counter></div>)
-    }
-
-
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+  render() {
+    console.log("props", this.props);
+    return (
+      <div>
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            value={counter.value}
+            selected={true}
+          ></Counter>
+        ))}
+      </div>
+    );
+  }
 }
 
-export default Counters
+export default Counters;
