7acd86c7d8e7e99e2cd68c5733d8c26511795859
holidayonmail@gmail.com <holidayonmail@gmail.com>
2021-01-31 02:13:07 +0300

    Added handleIncrement

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,43 +2,16 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-  handleIncrement = (counter) => {
-    const counters = this.state.counters.map((c) => {
-      if (c.id === counter) c.value += 1;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters });
-  };
   render() {
+    const { onReset, counters, onIncrement, onDelete } = this.props;
     return (
       <div>
-        <button onClick={this.handleReset}>Reset</button>
-        {this.state.counters.map((counter) => (
+        <button onClick={onReset}>Reset</button>
+        {counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
             counter={counter}
           />
         ))}
---
+++ src/components/navbar.jsx
@@ -0,0 +1,30 @@
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+// @flow
+
+// class NavBar extends Component {
+//   render() {
+//     return (
+//       <nav className="navbar navbar-light bg-light">
+//         <a className="navbar-brand" href="#">
+//           Navbar
+//           <span className="badge badge-pill badge-secondary">
+//             {this.props.totalCounters}
+//           </span>
+//         </a>
+//       </nav>
+//     );
+//   }
+// }
+
+export default NavBar;
